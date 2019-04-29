c7367d69d3df723c3d397555539696f959f5d892
yoav <yoavhortman5@gmail.com>
2019-04-29 10:08:10 +0200

    Fixed falling animation

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -8,18 +8,18 @@ const MAX_SPEED = 400;
 
 var motion = Vector2();
 func _physics_process(delta):
+	var animation = "Idle";
 	var isIdle = false;
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED);
 		$Sprite.flip_h = false;
-		$Sprite.play("Run");
+		animation = "Run";
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED);
 		$Sprite.flip_h = true;
-		$Sprite.play("Run");
+		animation = "Run";
 	else:
 		isIdle = true
-		$Sprite.play("Idle")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"): 
@@ -28,11 +28,11 @@ func _physics_process(delta):
 			motion.x = lerp(motion.x, 0, 0.1);
 	else:
 		if motion.y < 0:
-			$Sprite.play("Jump");
+			animation = "Jump";
 		elif motion.y > 0: 
-			$Sprite.play("Fall");
-		motion.y += GRAVITY;
-		
+			animation = "Fall";
+	
+	$Sprite.play(animation);
 	move_and_slide(motion, UP);
 
 
