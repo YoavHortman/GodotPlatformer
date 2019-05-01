5d084da0155c09533efd37ce4ab95a32a6c5b48f
Yoav <yoavhortman5@gmail.com>
2019-05-01 15:10:54 +0200

    .

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,16 +2,19 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1);
 const GRAVITY = 20;
-const ACCELERATION = 50;
+const ACCELERATION = 40;
 const JUMP_HEIGHT = -400;
 const MAX_SPEED = 400;
+const WALL_SLIDE_FRICTION = 0.9;
+const WALL_JUMP_FORCE = 200;
+
 
 var motion = Vector2();
 func _physics_process(delta):
 	var right = Input.is_action_pressed("ui_right");
 	var left =  Input.is_action_pressed("ui_left");
 	var jump = Input.is_action_just_pressed("ui_up");
-	
+
 	var animation = "Idle";
 	var isIdle = false;
 	motion.y += GRAVITY;
@@ -26,13 +29,18 @@ func _physics_process(delta):
 	else:
 		isIdle = true
 	
-	if $RayCastRight.is_colliding() && (motion.x > 0 || !$RayCastDown.is_colliding()):
-		motion.x = 0;
-		if jump:
-			motion.y = JUMP_HEIGHT / 1.5;
-			motion.x = -MAX_SPEED / 1.5;
-			$Sprite.flip_h = true;
 	
+	if (!$RayCastRightTop.is_colliding() && $RayCastRightTop2.is_colliding()):
+		motion.y = 0;
+		motion.x = 0;
+		if jump && !$RayCastDown.is_colliding():
+			motion.y = JUMP_HEIGHT;
+	elif $RayCastRightBottom.is_colliding() && motion.x >= 0:
+		wall_collision(-WALL_JUMP_FORCE, jump);
+	
+	if $RayCastLeft.is_colliding() && motion.x <= 0:
+		wall_collision(WALL_JUMP_FORCE, jump);
+		
 	if $RayCastDown.is_colliding():
 		motion.y = 0;
 		if jump:
@@ -44,12 +52,17 @@ func _physics_process(delta):
 			animation = "Jump";
 		else:
 			animation = "Fall";
-	motion = move_and_slide(motion, UP);
+	move_and_slide(motion, UP);
 	$Sprite.play(animation);
 
 func _ready():
 	print("ready");
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func wall_collision(opposite_force, jump):
+	motion.x = 0;
+	if motion.y > 0:
+		motion.y -= GRAVITY * WALL_SLIDE_FRICTION;
+		$Sprite.flip_h = opposite_force < 0;
+	if jump && !$RayCastDown.is_colliding():
+		motion.y = JUMP_HEIGHT;
+		motion.x = opposite_force;
